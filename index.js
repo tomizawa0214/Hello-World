ec5dedd53deb01296c6cf8bb2b55e9c9faec7cd8
tomizawa0214 <j.tomizawa.japan@gmail.com>
2020-02-02 19:51:59 +0900

    JSのテスト⑥

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(function() {
   $("#insert").click(function() {
-    let str= prompt("何か入力してください！");
+    let str= prompt("何か入力してください！")
     $("#dom").append($("<li>").text(str));
   });
 
@@ -10,5 +10,19 @@ $(function() {
 
   $(".remove").on('click' , function() {
     $(".ar-content").removeClass("ar-content-bg");
+  });
+
+  $(".show-content").on('click' , function() {
+    $(".ar-content").show();
+  });
+
+  $(".hide-content").on('click' , function() {
+    $(".ar-content").hide();
+  });
+
+  $("#modal").dialog({ autoOpen: false });
+
+  $("#openModal").click(function() {
+    $("#modal").dialog("open");
   });
 });
